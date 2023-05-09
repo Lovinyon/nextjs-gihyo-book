8686f77a84363bb1a63995c176ed57e5ba20720d
hiroto-hash <hiroto5118@icloud.com>
2023-05-10 07:10:30 +0900

    get articles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
-export default function Home() {
+import type { Article } from "./type";
+
+async function getArticles() {
+  const res = await fetch("http://localhost:3000/api/articles");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch articles");
+  }
+
+  const data = await res.json();
+  return data.articles as Article[];
+}
+
+export default async function Home() {
+  const articles = await getArticles();
+
   return (
     <div>
       <h1>新着記事</h1>
       <ul>
-        <li>記事1</li>
-        <li>記事2</li>
-        <li>記事3</li>
+        {articles.map((articles) => (
+          <li key={articles.id}>{articles.title}</li>
+        ))}
       </ul>
     </div>
   );
